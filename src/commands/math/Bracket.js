6229ccaec614dc36b270eb9c2fb2fc7d31905090
brentanalexander <brentan.alexander@gmail.com>
2015-10-16 11:16:25 -0700

    Add implicit multiply on ( next to a number

--- src/commands/math/Bracket.js
+++ src/commands/math/Bracket.js
@@ -79,7 +79,7 @@ var Bracket = P(P(MathCommand, DelimsMixin), function(_, super_) {
       if((!brack) && (this.ctrlSeq === '\\left(') && (cursor[L] instanceof Variable) && (this.side === L)) 
         return cursor[L].autoUnItalicize(cursor);
       else if((!brack) && (this.ctrlSeq === '\\left(') && (cursor[L] instanceof VanillaSymbol) && (cursor[L].ctrlSeq.match(/^[0-9]$/)) && (this.side === L)) 
-        LatexCmds.cdot().implicit().createLeftOf(cursor);\
+        LatexCmds.cdot().implicit().createLeftOf(cursor);
       else if((!brack) && (this.ctrlSeq === '\\left(') && (cursor[L] instanceof SupSub) && (this.side === L) && (cursor[L].supsub === 'sub') && (cursor[L]['sub'].ends[R] instanceof Variable)) {
         cursor.insAtRightEnd(cursor[L]['sub'])
         return cursor[L].autoUnItalicize(cursor); 
